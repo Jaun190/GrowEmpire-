e3cfc04e873003f404a2a1643ebd9e4918d233ba
Jaun190 <jaun190@gmail.com>
2025-06-24 19:03:00 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,8 @@
-// supabase.js – Verbindung zur Datenbank
+// supabase.js – Verbindung zur Supabase-Datenbank herstellen
 
-const SUPABASE_URL = 'https://ghfzdgaunxblpudkmxmy.supabase.co' const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdoZnpkZ2F1bnhibHB1ZGtteG15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3MDI5NTksImV4cCI6MjA2NjI3ODk1OX0.J8ni4SszaI33ljthdEjqXf8ZKFCs4uWnRZZDyU0IlUU'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
-const supabase = window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_URL = 'https://ghfzdgaunxblpudkmxmy.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdoZnpkZ2F1bnhibHB1ZGtteG15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3MDI5NTksImV4cCI6MjA2NjI3ODk1OX0.J8ni4SszaI33ljthdEjqXf8ZKFCs4uWnRZZDyU0IlUU'
 
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
